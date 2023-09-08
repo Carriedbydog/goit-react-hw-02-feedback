cbe865ac38ba67bb496dfff933d558b4d4597057
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-09-08 16:53:32 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { FeedBack } from './FeedBack/FeedBack';
+import React from 'react';
+
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <>
+          <FeedBack />
+        </>
+      </div>
+    );
+  }
+}
---
+++ src/components/FeedBack/FeedBack.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const FeedBack = () => {
+  return (
+    <div>
+      <h1>Please leave feedback</h1>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+      <h1>Statistics</h1>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
